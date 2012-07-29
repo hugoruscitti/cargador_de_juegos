7cd349e0b801a923c8f8bec4601abd53fb048e44
Hugo Ruscitti <hugoruscitti@gmail.com>
2012-07-29 13:56:11 -0300

    creando el primer cargador.

---
+++ cargador_basico/cargador.py
@@ -0,0 +1,12 @@
+import tkMessageBox
+import Tkinter
+import import
+
+try:
+    imp.load_source("__main__", "run.py")
+except IOError:
+    root = Tkinter.Tk()
+    root.withdraw()
+
+    MENSAJE_ERROR = "Lo siento, no se encuentra el archivo run.py"
+    tkMessageBox.showerror("Error", MENSAJE_ERROR)
