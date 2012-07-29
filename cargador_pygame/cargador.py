c666ec033f656d3c61f520a5ea693933b3159546
Hugo Ruscitti <hugoruscitti@gmail.com>
2012-07-29 17:51:35 -0300

    haciendo el cargador para pygame.

---
+++ cargador_pygame/cargador.py
@@ -0,0 +1,13 @@
+import pygame
+import tkMessageBox
+import Tkinter
+import imp
+
+try:
+    imp.load_source("__main__", "run.py")
+except IOError:
+    root = Tkinter.Tk()
+    root.withdraw()
+
+    MENSAJE_ERROR = "Lo siento, no se encuentra el archivo run.py"
+    tkMessageBox.showerror("Error", MENSAJE_ERROR)
