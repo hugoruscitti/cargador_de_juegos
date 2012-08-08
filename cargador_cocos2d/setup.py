b27ce90b826928eb76f09fe9cd2909f405713dd5
Hugo Ruscitti <hugoruscitti@gmail.com>
2012-08-08 19:42:40 -0300

    Agregando dependencias directamente en el archivo setup.py como dijo barajas

--- cargador_cocos2d/setup.py
+++ cargador_cocos2d/setup.py
@@ -4,11 +4,23 @@ from cx_Freeze import Executable
 exe = Executable(
         script="cargador.py",
         base="Win32GUI",
+
+        compress = True,
+        copyDependentFiles = True,
+        appendScriptToExe = False,
+        appendScriptToLibrary = False,
 )
+
+extra_options = {
+    "packages": ["cocos"],
+    "optimize" : 2,
+    "compressed":True,
+}
 
 setup(
     name="Cargador",
     version="0.1",
+    options = {"build_exe": extra_options},
     description="Un cargador de juegos",
     executables=[exe],
 )
--- cargador_pygame/setup.py
+++ cargador_pygame/setup.py
@@ -4,11 +4,20 @@ from cx_Freeze import Executable
 exe = Executable(
         script="cargador.py",
         base="Win32GUI",
+        compress = True,
+        copyDependentFiles = True,
 )
+
+extra_options = {
+    "packages": ["pygame"],
+    "optimize" : 2,
+    "compressed":True,
+}
 
 setup(
     name="Cargador",
     version="0.1",
+    options = {"build_exe": extra_options},
     description="Un cargador de juegos",
     executables=[exe],
 )
